b11622fbf7978e36dbb2203e6f69ddaff30c7618
Axel Schwenke <axel@mysql.com>
2012-05-10 14:18:07 +0200

    added sysbench skeleton

---
+++ sysbench-runs/skeleton/collect.sh
@@ -0,0 +1,6 @@
+for i in 1 2 4 8 16 32 64 128 256  ; 
+do cat res$1/sysbench.ro.$i.res | awk -v s=$i ' BEGIN { i=10 } /tps:/ { r=$5;tm=$12; if (i<1000) { r=$6;tm=$13 } ; print s,i,r,tm; i=i+10 } ' | tr -d ',ms' ; 
+done > sum.ro.$1.txt
+for i in 1 2 4 8 16 32 64 128 256  ; 
+do cat res$1/sysbench.rw.$i.res | awk -v s=$i ' BEGIN { i=10 } /tps:/ { r=$5;tm=$12; if (i<1000) { r=$6;tm=$13 } ; print s,i,r,tm; i=i+10 } ' | tr -d ',ms' ; 
+done > sum.rw.$1.txt
---
+++ sysbench-runs/skeleton/mkgraph.sh
@@ -0,0 +1,2 @@
+test -f graph.ro.pdf || R --no-save <graph.ro.R
+test -f graph.rw.pdf || R --no-save <graph.rw.R
