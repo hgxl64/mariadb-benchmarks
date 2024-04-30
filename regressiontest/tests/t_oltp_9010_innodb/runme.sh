9567e9b2ba7603c0734e1a913dbd0484778984ae
Axel Schwenke <axel@mariadb.com>
2024-04-30 21:06:42 +0200

    collect client side statistics with dstat

--- regressiontest/tests/t_oltp_9010_innodb/runme.sh
+++ regressiontest/tests/t_oltp_9010_innodb/runme.sh
@@ -87,8 +87,10 @@ mkdir -p ${LOGDIRECTORY}
     for thread in $THREADS
     do
        info -n " ${thread} ..."
-       numactl ${CPU_MASK_SYSBENCH:-"--all"} iostat -mx $REPORT $(($RUNTIME/$REPORT+1))  >> ${LOGDIRECTORY}/iostat.$thread.log &
+       $REMOTE_SHELL "iostat -mx $REPORT $(($RUNTIME/$REPORT+1))" >> ${LOGDIRECTORY}/iostat.$thread.log &
        PIDLIST=$!
+       numactl ${CPU_MASK_SYSBENCH:-"--all"} dstat --nocolor --noupdate $REPORT $(($RUNTIME/$REPORT)) >> ${LOGDIRECTORY}/dstat.$thread.log &
+       PIDLIST="$PIDLIST $!"
        if [[ -x ./dump_status.sh ]]
        then
            numactl ${CPU_MASK_SYSBENCH:-"--all"} ./dump_status.sh >> ${LOGDIRECTORY}/status.$thread.log &
