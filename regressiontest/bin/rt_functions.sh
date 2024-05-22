f0653d6362343a7f64f4ade6072b7b5aa497c31f
Axel Schwenke <axel@mariadb.com>
2024-05-22 18:21:09 +0200

    added newlines

--- regressiontest/bin/rt_functions.sh
+++ regressiontest/bin/rt_functions.sh
@@ -307,10 +307,10 @@ collect_host_info() {
 # log commit info to LOGDIR
 commit_info() {
 
-    git show -s --pretty="format:FULL_COMMIT: %H" $1  >  ${LOGDIRECTORY}/commit_info.yaml
-    git show -s --pretty="format:ABBRV_COMMIT: %h" $1 >> ${LOGDIRECTORY}/commit_info.yaml
-    git show -s --pretty="format:TIMESTAMP: %ct"  $1  >> ${LOGDIRECTORY}/commit_info.yaml
-    git show -s --pretty="format:RFC2822_TS: %cD" $1  >> ${LOGDIRECTORY}/commit_info.yaml
+    git show -s --pretty="format:FULL_COMMIT: %H%n"  $1 >  ${LOGDIRECTORY}/commit_info.yaml
+    git show -s --pretty="format:ABBRV_COMMIT: %h%n" $1 >> ${LOGDIRECTORY}/commit_info.yaml
+    git show -s --pretty="format:TIMESTAMP: %ct%n"   $1 >> ${LOGDIRECTORY}/commit_info.yaml
+    git show -s --pretty="format:RFC2822_TS: %cD%n"  $1 >> ${LOGDIRECTORY}/commit_info.yaml
 }
 
 
