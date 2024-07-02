17969def78f94d63cb9d2b112eb32da7944d5024
Axel Schwenke <axel@mariadb.com>
2024-07-02 11:45:17 +0200

    fixed indentation

--- regressiontest/bin/loop_community.sh
+++ regressiontest/bin/loop_community.sh
@@ -8,30 +8,30 @@ BRANCHES="10.5 10.6 10.11 11.1 11.2 11.4 11.5 11.6"
 STOPFILE="${HOME}/stop"
 IDLE=3600
 
-echo -e "running MariaDB CS regression tests\n\n'touch ${STOPFILE}' to stop gracefully\n"
-
 while true
 do
-  branches_tested=0
-  for branch in ${BRANCHES}
-  do
-    [[ -f ${STOPFILE} ]] && break
-    run_community_git.sh --branch ${branch} && branches_tested=$(($branches_tested + 1))
-  done
+    echo -e "running MariaDB CS regression tests\n\n'touch ${STOPFILE}' to stop gracefully\n"
+    branches_tested=0
 
-  if [[ -f ${STOPFILE} ]]
-  then
-    echo "found ${STOPFILE} - exiting"
-    break
-  fi
+    for branch in ${BRANCHES}
+    do
+        [[ -f ${STOPFILE} ]] && break
+        run_community_git.sh --branch ${branch} && branches_tested=$(($branches_tested + 1))
+    done
 
-  #if no branch had a new commit, sleep a while
-  if [[ $branches_tested -eq 0 ]]
-  then
-    date --utc "+%F %T no new commits in (${BRANCHES})"
-    echo "sleeping for ${IDLE} seconds"
-    sleep ${IDLE}
-  fi
+    if [[ -f ${STOPFILE} ]]
+    then
+        echo "found ${STOPFILE} - exiting"
+        break
+    fi
+
+    #if no branch had a new commit, sleep a while
+    if [[ $branches_tested -eq 0 ]]
+    then
+        date --utc "+%F %T no new commits in (${BRANCHES})"
+        echo "sleeping for ${IDLE} seconds"
+        sleep ${IDLE}
+    fi
 
 done
 
