bb4905861693c171204599b0b66757a8dd2a9e61
Axel Schwenke <axel@mariadb.com>
2024-11-19 18:00:39 +0100

    added documentation of used env variables

--- regressiontest/bin/dump_perf.sh
+++ regressiontest/bin/dump_perf.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
 # (w) Axel XL Schwenke for MariaDB Services AB
+#
+# relies on the following environment variables
+#
+# $PROFILEWARMUP ... time to sleep before profile is taken (default: 30s)
+# $PROFILETIME ..... time to collect the profile for (default: 60s)
+# $MYSQLD .......... the executable name of MariaDB/MySQL server
 
 set -e
 set -u
@@ -9,5 +15,5 @@ PROFILE=${1:?"missing PROFILE parameter!"}
 
 sleep ${PROFILEWARMUP:-30}
 
-perf record -F 400 -p $(pidof mariadbd) -o $PROFILE -g -- sleep ${PROFILETIME:-60}
+perf record -F 400 -p $(pidof ${MYSQLD}) -o $PROFILE -g -- sleep ${PROFILETIME:-60}
 
--- regressiontest/bin/dump_status.sh
+++ regressiontest/bin/dump_status.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 #
 # (w) Axel XL Schwenke for MariaDB Services AB
+#
+# relies on the following environment variables
+#
+# $RUNTIME ... execution time of the benchmark in seconds, 0 = endless
+# $REPORT .... interval between status reports in seconds
+# $SOCKET .... MariaDB/MySQL socket
+# $MYSQL ..... the executable name of MariaDB/MySQL command line client
 
-
-NRUN=$(($RUNTIME/$REPORT))
+# configuration
 
 QUERY="DO SLEEP($REPORT); SHOW GLOBAL STATUS;"
 USER="root"
 
+# no servicable parts behind this line
+
+NRUN=$(($RUNTIME/$REPORT))
 while [[ $RUNTIME -eq 0 || $NRUN -ge 0 ]]
 do
   $MYSQL -A -n -S $SOCKET -u $USER -e "$QUERY"
