13cd9ce335e5c128e135c079e5afa01ec7a4cd6d
Axel Schwenke <axel@mariadb.com>
2025-01-21 17:55:05 +0100

    fixes

--- regressiontest/experiments/TODO-5095.sh
+++ regressiontest/experiments/TODO-5095.sh
@@ -17,12 +17,12 @@ run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549
 #audit log for DDL/DCL, to file
 export EXTRAMYCNF="--plugin-load-add=server_audit2"
 export AUDIT_SQL=$(pwd)/audit.sql
-echo > ${AUDIT_SQL << EOT
-INSERT INTO server_audit_filters VALUES ('default', '{"logging": "OFF"}');
-INSERT INTO server_audit_filters VALUES ('log_ddl', '{"connect_event":"ALL","query_event":["DDL","DCL"],"table_event":["CREATE","DROP","RENAME","ALTER"]}');
-INSERT INTO server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_ddl');
+echo > $AUDIT_SQL <<EOT
+INSERT INTO mysql.server_audit_filters VALUES ('default', '{"logging": "OFF"}');
+INSERT INTO mysql.server_audit_filters VALUES ('log_ddl', '{"connect_event":"ALL","query_event":["DDL","DCL"],"table_event":["CREATE","DROP","RENAME","ALTER"]}');
+INSERT INTO mysql.server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_ddl');
 SET GLOBAL server_audit_reload_filters = ON;
-SET GLOBAL server_audit_output_type = FILE
+SET GLOBAL server_audit_output_type = FILE;
 SET GLOBAL server_audit_logging = ON;
 EOT
 run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549cc3ac5659f37ee8 --force
@@ -30,12 +30,12 @@ run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549
 #audit log for DDL/DCL/DML, to file
 export EXTRAMYCNF="--plugin-load-add=server_audit2"
 export AUDIT_SQL=$(pwd)/audit.sql
-echo > ${AUDIT_SQL << EOT
-INSERT INTO server_audit_filters VALUES ('default', '{"logging": "OFF"}');
-INSERT INTO server_audit_filters VALUES ('log_dml', '{"connect_event":"ALL","query_event":["DML","DDL","DCL","QUERY_DML_NO_SELECT"],"table_event":["WRITE","CREATE","DROP","RENAME","ALTER"]}');
-INSERT INTO server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_dml');
+echo > ${AUDIT_SQL} <<EOT
+INSERT INTO mysql.server_audit_filters VALUES ('default', '{"logging": "OFF"}');
+INSERT INTO mysql.server_audit_filters VALUES ('log_dml', '{"connect_event":"ALL","query_event":["DML","DDL","DCL","QUERY_DML_NO_SELECT"],"table_event":["WRITE","CREATE","DROP","RENAME","ALTER"]}');
+INSERT INTO mysql.server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_dml');
 SET GLOBAL server_audit_reload_filters = ON;
-SET GLOBAL server_audit_output_type = FILE
+SET GLOBAL server_audit_output_type = FILE;
 SET GLOBAL server_audit_logging = ON;
 EOT
 run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549cc3ac5659f37ee8 --force
@@ -43,12 +43,12 @@ run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549
 #audit log for everything, to file
 export EXTRAMYCNF="--plugin-load-add=server_audit2"
 export AUDIT_SQL=$(pwd)/audit.sql
-echo > ${AUDIT_SQL << EOT
-INSERT INTO server_audit_filters VALUES ('default', '{"logging": "OFF"}');
-INSERT INTO server_audit_filters VALUES ('log_all', '{"connect_event":"ALL","query_event":"ALL","table_event":"ALL"}');
-INSERT INTO server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_all');
+echo > ${AUDIT_SQL} <<EOT
+INSERT INTO mysql.server_audit_filters VALUES ('default', '{"logging": "OFF"}');
+INSERT INTO mysql.server_audit_filters VALUES ('log_all', '{"connect_event":"ALL","query_event":"ALL","table_event":"ALL"}');
+INSERT INTO mysql.server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_all');
 SET GLOBAL server_audit_reload_filters = ON;
-SET GLOBAL server_audit_output_type = FILE
+SET GLOBAL server_audit_output_type = FILE;
 SET GLOBAL server_audit_logging = ON;
 EOT
 run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549cc3ac5659f37ee8 --force
@@ -56,12 +56,12 @@ run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549
 #audit log for DDL/DCL, to syslog
 export EXTRAMYCNF="--plugin-load-add=server_audit2"
 export AUDIT_SQL=$(pwd)/audit.sql
-echo > ${AUDIT_SQL << EOT
-INSERT INTO server_audit_filters VALUES ('default', '{"logging": "OFF"}');
-INSERT INTO server_audit_filters VALUES ('log_ddl', '{"connect_event":"ALL","query_event":["DDL","DCL"],"table_event":["CREATE","DROP","RENAME","ALTER"]}');
-INSERT INTO server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_ddl');
+echo > ${AUDIT_SQL} <<EOT
+INSERT INTO mysql.server_audit_filters VALUES ('default', '{"logging": "OFF"}');
+INSERT INTO mysql.server_audit_filters VALUES ('log_ddl', '{"connect_event":"ALL","query_event":["DDL","DCL"],"table_event":["CREATE","DROP","RENAME","ALTER"]}');
+INSERT INTO mysql.server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_ddl');
 SET GLOBAL server_audit_reload_filters = ON;
-SET GLOBAL server_audit_output_type = SYSLOG
+SET GLOBAL server_audit_output_type = SYSLOG;
 SET GLOBAL server_audit_logging = ON;
 EOT
 run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549cc3ac5659f37ee8 --force
@@ -69,12 +69,12 @@ run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549
 #audit log for DDL/DCL/DML, to syslog
 export EXTRAMYCNF="--plugin-load-add=server_audit2"
 export AUDIT_SQL=$(pwd)/audit.sql
-echo > ${AUDIT_SQL << EOT
-INSERT INTO server_audit_filters VALUES ('default', '{"logging": "OFF"}');
-INSERT INTO server_audit_filters VALUES ('log_dml', '{"connect_event":"ALL","query_event":["DML","DDL","DCL","QUERY_DML_NO_SELECT"],"table_event":["WRITE","CREATE","DROP","RENAME","ALTER"]}');
-INSERT INTO server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_dml');
+echo > ${AUDIT_SQL} <<EOT
+INSERT INTO mysql.server_audit_filters VALUES ('default', '{"logging": "OFF"}');
+INSERT INTO mysql.server_audit_filters VALUES ('log_dml', '{"connect_event":"ALL","query_event":["DML","DDL","DCL","QUERY_DML_NO_SELECT"],"table_event":["WRITE","CREATE","DROP","RENAME","ALTER"]}');
+INSERT INTO mysql.server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_dml');
 SET GLOBAL server_audit_reload_filters = ON;
-SET GLOBAL server_audit_output_type = SYSLOG
+SET GLOBAL server_audit_output_type = SYSLOG;
 SET GLOBAL server_audit_logging = ON;
 EOT
 run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549cc3ac5659f37ee8 --force
@@ -82,12 +82,12 @@ run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549
 #audit log for everything, to syslog
 export EXTRAMYCNF="--plugin-load-add=server_audit2"
 export AUDIT_SQL=$(pwd)/audit.sql
-echo > ${AUDIT_SQL << EOT
-INSERT INTO server_audit_filters VALUES ('default', '{"logging": "OFF"}');
-INSERT INTO server_audit_filters VALUES ('log_all', '{"connect_event":"ALL","query_event":"ALL","table_event":"ALL"}');
-INSERT INTO server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_all');
+echo > ${AUDIT_SQL} <<EOT
+INSERT INTO mysql.server_audit_filters VALUES ('default', '{"logging": "OFF"}');
+INSERT INTO mysql.server_audit_filters VALUES ('log_all', '{"connect_event":"ALL","query_event":"ALL","table_event":"ALL"}');
+INSERT INTO mysql.server_audit_users (host, user, filtername) VALUES ('%', 'sysbench', 'log_all');
 SET GLOBAL server_audit_reload_filters = ON;
-SET GLOBAL server_audit_output_type = SYSLOG
+SET GLOBAL server_audit_output_type = SYSLOG;
 SET GLOBAL server_audit_logging = ON;
 EOT
 run_enterprise_jenkins.sh --branch 10.6.20-16 --commit 102842eee8608aebfc871f549cc3ac5659f37ee8 --force
