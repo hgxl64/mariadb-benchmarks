ef96120bd42eea30a775db3be97b03fb11168aa9
Axel Schwenke <axel@mariadb.com>
2026-03-31 22:21:40 +0200

    added post processing step

--- regressiontest/bin/run_installed_remote.sh
+++ regressiontest/bin/run_installed_remote.sh
@@ -123,5 +123,8 @@ mkdir -p $LOGDIRECTORY
     done
     date --utc "+%F %T" > $LOGDIRECTORY/stop
 
+    msg $(date --utc "+%F %T postprocess $(basename ${LOGDIRECTORY})")
+    run_postprocess.sh > $LOGDIRECTORY/postprocess.log
+
 } 2>&1 | tee $LOGDIRECTORY/${JOB}.log
 
