ca2cc7ff6c72ce280132e7cd90219ca72a29c412
Axel Schwenke <axel@mariadb.com>
2026-06-09 13:42:14 +0200

    fix

--- cloudbench/bin/load.data.sh
+++ cloudbench/bin/load.data.sh
@@ -317,7 +317,7 @@ time {
                         [[ ${STREAMS} ]] || STREAMS=${SYSBENCH_TABLES}
                         COMMAND="sysbench /data/cbench/driver/lua/${SYSBENCH_SCRIPT} $(get_sysbench_connection ${CLUSTER} ${HEADDRIVER})"
                         COMMAND="${COMMAND} --mysql-db=${SCHEMA}"
-                        COMMAND="${COMMAND} --table-size=${SYSBENCH_TABLE_SIZE} --tables=${SYSBENCH_TABLES} --threads=${STREAMS} ${SYSBENCH_OPTIONS}"
+                        COMMAND="${COMMAND} --table-size=${SYSBENCH_TABLESIZE} --tables=${SYSBENCH_TABLES} --threads=${STREAMS} ${SYSBENCH_OPTIONS}"
                         [[ ${OPTION_ENGINE} ]] && COMMAND="${COMMAND} --mysql_storage_engine=${OPTION_ENGINE}"
                         [[ ${OPTION_CHARSET} ]] && COMMAND="${COMMAND} --create_table_options=DEFAULT CHARSET=${OPTION_CHARSET}"
                         [[ ${OPTION_NOAUTOINC} ]] && COMMAND="${COMMAND} --auto-inc=off"
