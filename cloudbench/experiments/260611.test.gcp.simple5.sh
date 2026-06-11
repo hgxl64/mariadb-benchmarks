58733a8688e978c885e0be1ebfdd44586a0fae6a
Axel Schwenke <axel@mariadb.com>
2026-06-11 21:43:45 +0200

    redefined test5

--- cloudbench/experiments/260611.test.gcp.simple5.sh
+++ cloudbench/experiments/260611.test.gcp.simple5.sh
@@ -5,7 +5,7 @@ source ${CBENCH_HOME}/config/gcp.conf
 
 export CLUSTER='test'
 
-TEST_NAME="gcp.test.simple4"
+TEST_NAME="gcp.test.simple5"
 [[ ${TESTID} ]] || TESTID=$(date +%y%m%d.%H%M%S).${TEST_NAME}
 if [[ ! ${LOGDIRECTORY} ]] ; then
     export LOGDIRECTORY=${CBENCH_LOG_HOME}/${TESTID}
@@ -16,27 +16,30 @@ mkdir -p ${LOGDIRECTORY}
 
 {
     gcp.allocate.nodes.sh --cluster ${CLUSTER} \
-     --server-nodes 3 --server-type n2-standard-4 \
-     --driver-nodes 1 --driver-type n2-standard-2
+     --server-nodes 3 --server-type n2-standard-8 \
+     --driver-nodes 2 --driver-type n2-standard-2 \
+     --maxscale-nodes 1 --maxscale-type n2-standard-8
 
     SYSTEMS=( $(get_property ${CLUSTER} systems) )
     echo
     echo "        SYSTEMS = ${SYSTEMS[*]}"
     [[ ${SYSTEMS} ]] || { echo "ERROR Unable to allocate nodes."; exit 1; }
 
-    configure.cluster.sh --cluster ${CLUSTER} --cluster-type galera_mastermaster \
+    configure.cluster.sh --cluster ${CLUSTER} --cluster-type galera_masterslave \
      --master-system ${CLUSTER}-server-1 --slave-system ${CLUSTER}-server-2 \
-     --slave-system ${CLUSTER}-server-3 --driver-system ${CLUSTER}-driver-1
+     --slave-system ${CLUSTER}-server-3 --maxscale-system ${CLUSTER}-maxscale-1 \
+     --driver-system ${CLUSTER}-driver-1 --driver-system ${CLUSTER}-driver-2
 
     echo "Properties File:"
     showproperties
 
     build.cluster.sh --cluster ${CLUSTER}
 
-    echo "Version : $(get_database_version ${CLUSTER})"
+    echo "Version (direct) :          $(get_database_version ${CLUSTER})"
+    echo "Version (through MaxScale): $(get_database_version ${CLUSTER}.maxscale)"
 
     sysbench.load.sh --cluster ${CLUSTER} --skipcheck --noautoinc --load
-    sysbench.curve.sh --cluster ${CLUSTER} --skipcheck --workload oltp_read_write --start_streams 4
+    sysbench.curves.sh --cluster ${CLUSTER}.maxscale --workload 7525_splittable --start_streams 4 --repeats 3
 
     gcp.release.nodes.sh --cluster ${CLUSTER}
 
