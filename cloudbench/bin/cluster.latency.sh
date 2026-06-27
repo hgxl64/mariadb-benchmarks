5ebeb6492c715fa84a92e5946ef8bc985c8c56d0
Axel Schwenke <axel@mariadb.com>
2026-06-27 12:02:42 +0200

    PERF-531 - add node latencies

---
+++ cloudbench/bin/cluster.latency.sh
@@ -0,0 +1,292 @@
+#!/bin/bash
+
+source ${CBENCH_HOME}/bin/cbench.sh
+
+USAGE="usage: $0
+
+    Set, reset or show network latency between the server nodes of a cluster.
+    The latencies are stored in cluster.latency.properties
+
+    Or check the latencies between all types of nodes. This does not require
+    cluster.latency.properties
+
+    Parameters:
+        ...
+
+        [ -h|--help ]
+
+    Notes:
+"
+
+COMMAND_LINE="$@"
+
+while [[ $# > 0 ]] ; do
+    key="$1"; shift;
+    case ${key} in
+
+        --cluster)              CLUSTER="$1"; shift;;
+
+        --check)                MODE="check";;
+        --test)                 MODE="check";;
+        --reset)                MODE="reset";;
+        --set)                  MODE="set";;
+        --show)                 MODE="show";;
+
+        -h|--help) echo -e "$USAGE"; exit 1 ;;
+        *) echo "Invalid input switch: $key"; echo -e "$0 ${COMMAND_LINE}"; echo -e "$USAGE"; exit 1 ;;
+    esac
+done
+
+[[ ${CLUSTER} ]] || error "no CLUSTER given, aborting"
+
+[[ ${MODE} ]] || MODE="check"
+
+# logging
+TEST_NAME=cluster.latency
+if [[ ! ${TESTID} ]] ; then TESTID=$(date +%y%m%d.%H%M%S).${CLUSTER}; fi
+if [[ ! ${LOGDIRECTORY} ]] ; then
+    export LOGDIRECTORY=${CBENCH_LOG_HOME}/${CLUSTER}/${TESTID}.${TEST_NAME}
+else
+    LOGDIRECTORY=${LOGDIRECTORY}/$(date +%y%m%d.%H%M%S%3N).${TEST_NAME}
+fi
+mkdir -p ${LOGDIRECTORY}
+
+{
+    echo
+    echo "    ===== Begin $0 =====  [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+    echo
+    echo "        $0 ${COMMAND_LINE}"
+    echo
+    echo "        CLUSTER        = ${CLUSTER}"
+    echo
+
+    time {
+
+        case ${MODE} in
+
+            check)
+                echo "    ===== Checking Network Latency ===== [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+                echo
+
+                #collect list of server systems
+                case $(get_property ${CLUSTER} cluster.type) in
+                    mariadb_replication)
+                        SERVER_SYSTEMS=( $(get_property ${CLUSTER} master.systems) $(get_property ${CLUSTER} slave.systems) );;
+                    galera_*)
+                        SERVER_SYSTEMS=( $(get_property ${CLUSTER} galera.systems) );;
+                    raft_*)
+                        SERVER_SYSTEMS=( $(get_property ${CLUSTER} raft.systems) );;
+                    *)
+                        SERVER_SYSTEMS=( $(get_property ${CLUSTER} mariadb.systems) );;
+                esac
+                #and maxscale systems
+                MAXSCALE_SYSTEMS=( $(get_property ${CLUSTER} maxscale.systems) )
+                #and driver systems
+                DRIVER_SYSTEMS=( $(get_property ${CLUSTER} driver.systems) )
+
+                echo "        SERVER_SYSTEMS    = ${SERVER_SYSTEMS[*]}"
+                echo "        MAXSCALE_SYSTEMS  = ${MAXSCALE_SYSTEMS[*]}"
+                echo "        DRIVER_SYSTEMS    = ${DRIVER_SYSTEMS[*]}"
+                echo
+
+                echo "        ===== Ping Test ====="
+                echo
+                time {
+                    unset PIDLIST
+                    #each server to each other
+                    for ORIGIN in ${SERVER_SYSTEMS[*]} ; do
+                        for TARGET in ${SERVER_SYSTEMS[*]} ; do
+                            TARGET_IP=$(get_property ${TARGET} system.internal.ip)
+                            ssh $(get_ssh_connection ${CLUSTER} ${ORIGIN}) "ping -A -c 10 ${TARGET_IP}" > ${LOGDIRECTORY}/ping.${ORIGIN}.to.${TARGET}.log &
+                            PIDLIST="${PIDLIST} $!"
+                        done
+                    done
+                    #maxscales to servers
+                    for ORIGIN in ${MAXSCALE_SYSTEMS[*]} ; do
+                        for TARGET in ${SERVER_SYSTEMS[*]} ; do
+                            TARGET_IP=$(get_property ${TARGET} system.internal.ip)
+                            ssh $(get_ssh_connection ${CLUSTER} ${ORIGIN}) "ping -A -c 10 ${TARGET_IP}" > ${LOGDIRECTORY}/ping.${ORIGIN}.to.${TARGET}.log &
+                            PIDLIST="${PIDLIST} $!"
+                        done
+                    done
+                    #drivers to servers and to maxscales
+                    for ORIGIN in ${DRIVER_SYSTEMS[*]} ; do
+                        for TARGET in ${SERVER_SYSTEMS[*]} ${MAXSCALE_SYSTEMS[*]} ; do
+                            TARGET_IP=$(get_property ${TARGET} system.internal.ip)
+                            ssh $(get_ssh_connection ${CLUSTER} ${ORIGIN}) "ping -A -c 10 ${TARGET_IP}" > ${LOGDIRECTORY}/ping.${ORIGIN}.to.${TARGET}.log &
+                            PIDLIST="${PIDLIST} $!"
+                        done
+                    done
+                    #wait for everyone to finish
+                    wait ${PIDLIST}
+                    unset PIDLIST
+                }
+
+                echo
+                echo "----- average rtt between server nodes in ms:"
+                echo
+                {
+                    echo "from\\to ${SERVER_SYSTEMS[*]}"
+                    for ORIGIN in ${SERVER_SYSTEMS[*]} ; do
+                        echo -n "${ORIGIN}"
+                        for TARGET in ${SERVER_SYSTEMS[*]} ; do
+                            echo -n " $(tail -1 ${LOGDIRECTORY}/ping.${ORIGIN}.to.${TARGET}.log | cut -d= -f2 | cut -d/ -f2)"
+                        done
+                        echo
+                    done
+                } | column -t
+
+                [[ ${MAXSCALE_SYSTEMS[*]} ]] && {
+                    echo
+                    echo "----- average rtt between maxscale nodes and servers in ms:"
+                    echo
+                    {
+                        echo "from\\to ${SERVER_SYSTEMS[*]}"
+                        for ORIGIN in ${MAXSCALE_SYSTEMS[*]} ; do
+                            for TARGET in ${SERVER_SYSTEMS[*]} ; do
+                                echo -n " $(tail -1 ${LOGDIRECTORY}/ping.${ORIGIN}.to.${TARGET}.log | cut -d= -f2 | cut -d/ -f2)"
+                            done
+                        done
+                    } | column -t
+                }
+
+                [[ ${DRIVER_SYSTEMS[*]} ]] && {
+                    echo
+                    echo "----- average rtt between driver nodes and servers in ms:"
+                    echo
+                    {
+                        echo "from\\to ${SERVER_SYSTEMS[*]}"
+                        for ORIGIN in ${DRIVER_SYSTEMS[*]} ; do
+                            for TARGET in ${SERVER_SYSTEMS[*]} ; do
+                                echo -n " $(tail -1 ${LOGDIRECTORY}/ping.${ORIGIN}.to.${TARGET}.log | cut -d= -f2 | cut -d/ -f2)"
+                            done
+                        done
+                    } | column -t
+                    [[ ${MAXSCALE_SYSTEMS[*]} ]] && {
+                        echo
+                        echo "----- average rtt between driver nodes and maxscales in ms:"
+                        echo
+                        {
+                            echo "from\\to ${SERVER_SYSTEMS[*]} ${MAXSCALE_SYSTEMS[*]"
+                            for ORIGIN in ${DRIVER_SYSTEMS[*]} ; do
+                                for TARGET in ${SERVER_SYSTEMS[*]} ${MAXSCALE_SYSTEMS[*]} ; do
+                                    echo -n " $(tail -1 ${LOGDIRECTORY}/ping.${ORIGIN}.to.${TARGET}.log | cut -d= -f2 | cut -d/ -f2)"
+                                done
+                            done
+                        } | column -t
+                    }
+                }
+
+                ;;
+
+            reset)
+                ALL_SYSTEMS=$(get_property ${CLUSTER}.latency all.systems)
+                [[ ${ALL_SYSTEMS} ]] || error "no systems found. Did you configure latency?"
+
+                echo "    ===== Resetting Network Latency ===== [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+                echo
+                echo "        ALL_SYSTEMS    = ${ALL_SYSTEMS}"
+                echo
+
+                for ORIGIN in ${ALL_SYSTEMS[*]} ; do
+                    echo -n "${ORIGIN} : "
+                    ssh $(get_ssh_connection ${CLUSTER} ${ORIGIN}) '
+                        IP="'$(get_property ${ORIGIN} system.internal.ip)'"
+                        NETDEV=$(ip -o addr show | fgrep "${IP}" | awk "{print \$2}")
+                        if [[ ${NETDEV} ]] ; then
+                            sudo tc qdisc del dev ${NETDEV} root &> /dev/null
+                            echo "qdisc cleared for ip=${IP}, device=${NETDEV}"
+                        else
+                            echo "could not find net device for ip=${IP}"
+                        fi
+                    '
+                done
+                ;;
+
+            show)
+                ALL_SYSTEMS=$(get_property ${CLUSTER}.latency all.systems)
+                [[ ${ALL_SYSTEMS} ]] || error "no systems found. Did you configure latency?"
+
+                echo "    ===== Configured Network Latency ===== [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+                echo
+                echo "        ALL_SYSTEMS    = ${ALL_SYSTEMS}"
+                echo
+
+                echo "configured network latency  between nodes:"
+                echo
+                {
+                    echo "from\\to ${ALL_SYSTEMS[*]}"
+                    for ORIGIN in ${ALL_SYSTEMS[*]} ; do
+                        echo "${ORIGIN} $(get_property ${CLUSTER}.latency ${ORIGIN}.latency)"
+                    done
+                } | column -t
+                ;;
+
+            set)
+                ALL_SYSTEMS=$(get_property ${CLUSTER}.latency all.systems)
+                [[ ${ALL_SYSTEMS} ]] || error "no systems found. Did you configure latency?"
+
+                echo "    ===== Setting Network Latency ===== [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+                echo
+                echo "        ALL_SYSTEMS    = ${ALL_SYSTEMS}"
+                echo
+
+                for ORIGIN in ${ALL_SYSTEMS[*]} ; do
+
+                    unset TARGET_IPS
+                    unset TARGET_LATENCY
+                    ORIGIN_IP=$(get_property ${ORIGIN} system.internal.ip)
+                    LATENCIES=$(get_property ${CLUSTER}.lateny ${ORIGIN}.latency)
+                    for ((IDX=0; IDX<${#ALL_SYSTEMS[*]}; IDX++)) ; do
+                        if [[ ${ORIGIN} != ${ALL_SYSTEMS[$IDX]} ]] ; then
+                            TARGET_IPS=( ${TARGET_IPS[*]} ${ALL_SYSTEMS[$IDX]} )
+                            TARGET_LATENCY=( ${TARGET_LATENCY[*]} ${LATENCIES[$IDX]} )
+                        fi
+                    done
+
+                    echo -n "${ORIGIN} : "
+                    ssh $(get_ssh_connection ${CLUSTER} ${ORIGIN}) '
+                        ORIGIN_IP="'${ORIGIN_IP}'"
+                        TARGET_IPS=( "'${TARGET_IPS[*]}'" )
+                        TARGET_LATENCY=( "'${TARGET_LATENCY[*]}'" )
+                        BANDWIDTH="16gbit"
+                        NETDEV=$(ip -o addr show | fgrep "${ORIGIN_IP}" | awk "{print \$2}")
+                        if [[ ${NETDEV} ]] ; then
+                            #delete any existing rules
+                            sudo tc qdisc del dev ${NETDEV} root &> /dev/null
+                            echo "qdisc cleared for ip=${IP}, device=${NETDEV}"
+                            #setup parent HTB
+                            sudo tc qdisc add dev ${NETDEV} root handle 1: htb default 999
+                            sudo tc class add dev ${NETDEV} parent 1: classid 1:1 htb rate ${BANDWIDTH} ceil ${BANDWIDTH}
+                            sudo tc class add dev ${NETDEV} parent 1:1 classid 1:999 htb rate ${BANDWIDTH} ceil ${BANDWIDTH}
+                            sudo tc qdisc add dev ${NETDEV} parent 1:999 handle 2: fq_codel
+                            echo "  parent HTB added with bandwidth ${BANDWIDTH}"
+                            #setup HTB for each target
+                            for (( IDX=0; IDX<${#TARGET_IPS[*]}; IDX++ )) ; do
+                                LATENCY=${TARGET_LATENCY[$IDX]}
+                                (( ${LATENCY} == 0 )) && continue
+                                TARGET=${TARGET_IPS[$IDX]}
+                                (( MINOR=10 + IDX ))
+                                sudo tc class add dev ${NETDEV} parent 1:1 classid 1:${MINOR} htb rate ${BANDWIDTH} ceil ${BANDWIDTH} prio 1
+                                sudo tc qdisc add dev ${NETDEV} parent 1:${MINOR} handle ${MINOR}0: netem delay ${LATENCY} limit 1000
+                                sudo tc filter add dev ${NETDEV} protocol ip parent 1: prio 1 u32 match ip dst ${TARGET} flowid 1:${MINOR}
+                                echo "  netem delay ${LATENCY} added for dst ${TARGET}"
+                            done
+                        else
+                            echo "could not find net device for ip=${ORIGIN_IP}"
+                        fi
+                    '
+                done
+
+                ;;
+
+            *) error "Unsupported Operation Mode: '${MODE}'"
+        esac
+
+    }
+
+    echo
+    echo "    ===== End $0 =====  [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+    echo
+
+} 2>&1 | tee ${LOGDIRECTORY}/$(date +%y%m%d.%H%M%S%3N).${TEST_NAME}.log
---
+++ cloudbench/bin/configure.latency.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+source ${CBENCH_HOME}/bin/cbench.sh
+
+USAGE="usage: $0
+
+    Configure network latency between the server nodes of a cluster.
+    The latencies are stored in cluster.latency.properties
+
+    Cluster Types:  mariadb_replication, galera_masterslave, galera_mastermaster
+
+    Parameters:
+        ...
+
+        [ -h|--help ]
+
+    Notes:
+"
+
+COMMAND_LINE="$@"
+
+unset SLOW_SYSTEMS
+
+while [[ $# > 0 ]] ; do
+    key="$1"; shift;
+    case ${key} in
+
+        --cluster)              CLUSTER="$1"; shift;;
+
+        --latency)              LATENCY="$1"; shift;;
+
+        --slow)                 SLOW_SYSTEMS=( ${SLOW_SYSTEMS[*]} "$1" ); shift;;
+        --slow-node)            SLOW_SYSTEMS=( ${SLOW_SYSTEMS[*]} "$1" ); shift;;
+
+        -h|--help) echo -e "$USAGE"; exit 1 ;;
+        *) echo "Invalid input switch: $key"; echo -e "$0 ${COMMAND_LINE}"; echo -e "$USAGE"; exit 1 ;;
+    esac
+done
+
+[[ ${CLUSTER} ]] || error "no CLUSTER given, aborting"
+[[ ${LATENCY} ]] || error "no LATENCY given, nothing to do"
+
+# logging
+TEST_NAME=configure.latency
+if [[ ! ${TESTID} ]] ; then TESTID=$(date +%y%m%d.%H%M%S).${CLUSTER}; fi
+if [[ ! ${LOGDIRECTORY} ]] ; then
+    export LOGDIRECTORY=${CBENCH_LOG_HOME}/${CLUSTER}/${TESTID}.${TEST_NAME}
+else
+    LOGDIRECTORY=${LOGDIRECTORY}/$(date +%y%m%d.%H%M%S%3N).${TEST_NAME}
+fi
+mkdir -p ${LOGDIRECTORY}
+
+{
+    echo
+    echo "    ===== Begin $0 =====  [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+    echo
+    echo "        $0 ${COMMAND_LINE}"
+    echo
+
+    time {
+
+        CLUSTER_TYPE=$(get_property ${CLUSTER} cluster.type)
+        unset ALL_SYSTEMS
+
+        case ${CLUSTER_TYPE} in
+            mariadb_replication)
+                ALL_SYSTEMS=( $(get_property ${CLUSTER} master.systems) $(get_property ${CLUSTER} slave.systems) )
+                ;;
+
+            galera_*)
+                ALL_SYSTEMS=( $(get_property ${CLUSTER} galera.systems) )
+                ;;
+
+            raft_*)
+                ALL_SYSTEMS=( $(get_property ${CLUSTER} raft.systems) )
+                ;;
+
+            *) error "Unsupported Cluster Type: CLUSTER_TYPE = ${CLUSTER_TYPE}"
+        esac
+
+        IFS=$'\n'
+        ALL_SYSTEMS=( $(sort <<< "${ALL_SYSTEMS[*]}") )
+        SLOW_SYSTEMS=( $(sort <<< "${SLOW_SYSTEMS[*]}") )
+        unset IFS
+
+        echo "    ===== Generating Latency Matrix ===== [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+        echo
+        echo "        CLUSTER        = ${CLUSTER}"
+        echo "        CLUSTER_TYPE   = ${CLUSTER_TYPE}"
+        echo
+        echo "        ALL_SYSTEMS    = ( ${ALL_SYSTEMS[*]} )"
+        echo "        SLOW_SYSTEMS   = ( ${SLOW_SYSTEMS[*]} )"
+        echo "        LATENCY        = ${LATENCY}"
+        echo
+
+        {
+            echo
+            echo "cluster.name = ${CLUSTER}"
+            echo "cluster.type = ${CLUSTER_TYPE}"
+            echo
+            echo "all.systems = ${ALL_SYSTEMS[*]}"
+            echo "slow.systems = ${SLOW_SYSTEMS[*]}"
+            echo
+
+            NUM_SYSTEMS=${#ALL_SYSTEMS[*]}
+            for (( IDX1=0; IDX1<$NUM_SYSTEMS; IDX1++ )) ; do
+                SYSTEM=${ALL_SYSTEMS[$IDX1]}
+                echo -n "${SYSTEM}.latency ="
+                if [[ " ${SLOW_SYSTEMS[*]} " =~ " ${SYSTEM} " ]]; then
+                    #äthis is  slow system, set latency exept for self
+                    for (( IDX2=0; IDX2<$NUM_SYSTEMS; IDX2++ )) ; do
+                        if (( ${IDX1} == ${IDX2} )) ; then
+                            echo -n " 0"
+                        else
+                            echo -n " ${LATENCY}"
+                        fi
+                    done
+                else
+                    #this is a fast system, set latency only when other system is slow
+                    for (( IDX2=0 ; IDX2<${#ALL_SYSTEMS[@]} ; IDX2++ )) ; do
+                        if [[ " ${SLOW_SYSTEMS[*]} " =~ " ${ALL_SYSTEMS[$IDX2]} " ]] ; then
+                            echo -n " ${LATENCY}"
+                        else
+                            echo -n " 0"
+                        fi
+                    done
+                fi
+                echo
+            done
+            echo
+        } | tee properties/${CLUSTER}.latency.properties
+    }
+
+    echo
+    echo "    ===== End $0 =====  [ $(date -u '+%Y-%m-%d %H:%M:%S.%3N') ]"
+    echo
+
+} 2>&1 | tee ${LOGDIRECTORY}/$(date +%y%m%d.%H%M%S%3N).${TEST_NAME}.log
