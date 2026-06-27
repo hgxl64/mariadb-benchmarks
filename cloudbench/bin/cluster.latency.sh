e1a35625eb077e08819e908baff6b6b319eebf7d
Axel Schwenke <axel@mariadb.com>
2026-06-27 13:01:24 +0200

    fix

--- cloudbench/bin/cluster.latency.sh
+++ cloudbench/bin/cluster.latency.sh
@@ -194,7 +194,7 @@ mkdir -p ${LOGDIRECTORY}
                             echo "to-> ${SERVER_SYSTEMS[*]} ${MAXSCALE_SYSTEMS[*]}"
                             for ORIGIN in ${DRIVER_SYSTEMS[*]} ; do
                                 echo -n "${ORIGIN}"
-                                for TARGET in ${SERVER_SYSTEMS[*]} ${MAXSCALE_SYSTEMS[*]} ; do
+                                for TARGET in ${MAXSCALE_SYSTEMS[*]} ; do
                                     echo -n " $(tail -1 ${LOGDIRECTORY}/ping.${ORIGIN}.to.${TARGET}.log | cut -d= -f2 | cut -d/ -f2)"
                                 done
                                 echo
