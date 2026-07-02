7544fd16a65c3e80306b85489e302029d1524d65
Axel Schwenke <axel@mariadb.com>
2026-07-02 17:07:36 +0200

    added Grafana helper scripts

---
+++ cloudbench/bin/start.grafana.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ${CBENCH_HOME}/bin/cbench.sh
+
+start_grafana;
+
+export GRAFANA_START
+
---
+++ cloudbench/bin/stop.grafana.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+source ${CBENCH_HOME}/bin/cbench.sh
+
+USAGE="usage: $0
+    Snapshot preconfigured Grafana dashboards
+
+    Parameters:
+        --cluster
+        [ -h|--help ]
+
+    Notes:
+"
+
+COMMAND_LINE="$@"
+
+while [[ $# > 0 ]] ; do
+    key="$1"; shift;
+    case ${key} in
+        --cluster)              CLUSTER="$1"; shift;;
+        -h|--help) echo -e "$USAGE"; exit 1 ;;
+        *) echo "Invalid input switch: $key"; echo -e "$0 ${COMMAND_LINE}"; echo -e "$USAGE"; exit 1 ;;
+    esac
+done
+
+[[ ${CLUSTER} ]] || error "no CLUSTER given, aborting"
+
+# logging
+TEST_NAME=stop.grafana
+if [[ ! ${TESTID} ]] ; then TESTID=$(date +%y%m%d.%H%M%S).${CLUSTER}; fi
+if [[ ! ${LOGDIRECTORY} ]] ; then
+    export LOGDIRECTORY=${CBENCH_LOG_HOME}/${CLUSTER}/${TESTID}.${TEST_NAME}
+else
+    LOGDIRECTORY=${LOGDIRECTORY}/$(date +%y%m%d.%H%M%S%3N).${TEST_NAME}
+fi
+mkdir -p ${LOGDIRECTORY}
+
+
+stop_grafana 2>&1 | tee ${LOGDIRECTORY}/$(date +%y%m%d.%H%M%S%3N).${TEST_NAME}.log
+
+unset GRAFANA_START
+
