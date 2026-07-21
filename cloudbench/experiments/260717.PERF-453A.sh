66fe3a380485249bb3e1516399d3a4a30a353311
Axel Schwenke <axel@mariadb.com>
2026-07-21 21:47:55 +0200

    fix

--- cloudbench/experiments/260717.PERF-453A.sh
+++ cloudbench/experiments/260717.PERF-453A.sh
@@ -201,7 +201,9 @@ run_product() {
                 for LAT in ${LATENCIES[*]} ; do
                     sleep $((SLEEPTIME - 1))
                     COMMAND="configure.latency.sh --cluster ${CLUSTER} --latency ${LAT}"
-                    COMMAND="${COMMAND} --slow-node '${SLOW_NODES}'"
+                    for N in ${SLOW_NODES} ; do
+                        COMMAND="${COMMAND} --slow-node ${N}"
+                    done
                     exec_no_output ${COMMAND}
                     COMMAND="cluster.latency.sh --cluster ${CLUSTER} --show --set --check"
                     exec_no_output ${COMMAND} &
